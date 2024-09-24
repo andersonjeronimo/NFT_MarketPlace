1ef7e69c28a7ad2d32528373c810b57dc43ccea0
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2024-09-23 22:52:53 -0300

    NFT Collection smart contract

---
+++ blockchain/contracts/NFTCollection.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTCollection is ERC721URIStorage {
+    uint256 private _tokenIds;
+    address contractAddress; //marketplace
+    address owner;
+
+    constructor(address marketplaceAddress) ERC721("ProtoOpenSea", "POS") {
+        contractAddress = marketplaceAddress;
+        owner = msg.sender;
+    }
+
+    function mint(string memory uri) public returns (uint) {
+        _tokenIds++;
+        uint tokenId = _tokenIds;
+
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        setApprovalForAll(contractAddress, true);
+
+        return tokenId;
+    }
+
+    function setApprovalForAll(
+        address operator,
+        bool approved
+    ) public virtual override(ERC721, IERC721) {
+        require(
+            _msgSender() == owner || operator != contractAddress || approved,
+            "Cannot remove marketplace approval"
+        );
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+}
