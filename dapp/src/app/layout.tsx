d38bd3c309c940ebea4a136feaa400430a23278b
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2024-10-01 22:13:28 -0300

    Next.js init, theme setup -> Tailwind.css

---
+++ dapp/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "NFT Marketplace",
+  description: "NFC Marketplace OpenSea® like",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      {/* <body className="text-gray-500">{children}</body> */}
+    </html>
+  );
+}
