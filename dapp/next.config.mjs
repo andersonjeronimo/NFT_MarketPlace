e67cb29a7ffe2ee485707eed2901b04eef7370b0
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2024-10-04 22:35:01 -0300

    Theme setup

--- dapp/next.config.mjs
+++ dapp/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        MARKETPLACE_ADDRESS: process.env.MARKETPLACE_ADDRESS,
+        COLLECTION_ADDRESS: process.env.COLLECTION_ADDRESS,
+        CHAIN_ID: process.env.CHAIN_ID,
+        API_KEY: process.env.API_KEY,
+        API_SECRET: process.env.API_SECRET
+    }
+};
 
 export default nextConfig;
