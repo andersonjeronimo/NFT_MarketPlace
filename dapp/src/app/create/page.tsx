f4d7fca5f5b9f1c00641c4df62207afc857148b8
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2024-10-09 20:41:08 -0300

    Layout components

---
+++ dapp/src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+function Create() {
+    return (<>
+        <Header />
+        <Footer />
+    </>)
+}
+
+export default Create;
---
+++ dapp/src/app/pages/account/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "@/components/header";
+
+function Account() {
+    return (<Header />)
+}
+
+export default Account;
